e7602d88ba0a6c6eb891a24f6a6940f550814f21
real-StanYan <stanhavenoidea@gmail.com>
2023-08-31 15:28:38 +1000

    fix

--- app/api/blog/new/route.js
+++ app/api/blog/new/route.js
@@ -1,54 +1,54 @@
-import Blog from "@/models/blog";
-import { connectToDB } from "@/utils/database";
-import multer from "multer";
+// import Blog from "@/models/blog";
+// import { connectToDB } from "@/utils/database";
+// import multer from "multer";
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
 
-const uploadSingle = upload.single("image");
+// const uploadSingle = upload.single("image");
 
-const multerPromise = (req) => {
-  return new Promise((resolve, reject) => {
-    uploadSingle(req, {}, (err) => {
-      if (err) {
-        reject({ message: "Error uploading image" });
-      } else {
-        resolve(req);
-      }
-    });
-  });
-};
+// const multerPromise = (req) => {
+//   return new Promise((resolve, reject) => {
+//     uploadSingle(req, {}, (err) => {
+//       if (err) {
+//         reject({ message: "Error uploading image" });
+//       } else {
+//         resolve(req);
+//       }
+//     });
+//   });
+// };
 
-export const POST = async (request, response) => {
-  try {
-    await multerPromise(request);
+// export const POST = async (request, response) => {
+//   try {
+//     await multerPromise(request);
 
-    const blog = request.body.blog;
-    const imgData = request.file ? request.file.buffer : null;
-    const imgContentType = request.file ? request.file.mimetype : null;
+//     const blog = request.body.blog;
+//     const imgData = request.file ? request.file.buffer : null;
+//     const imgContentType = request.file ? request.file.mimetype : null;
 
-    await connectToDB();
+//     await connectToDB();
 
-    const newBlog = new Blog({
-      blog,
-      img: {
-        data: imgData,
-        contentType: imgContentType,
-      },
-    });
+//     const newBlog = new Blog({
+//       blog,
+//       img: {
+//         data: imgData,
+//         contentType: imgContentType,
+//       },
+//     });
 
-    await newBlog.save();
+//     await newBlog.save();
 
-    response.statusCode = 201;
-    response.json(newBlog);
-  } catch (error) {
-    response.statusCode = 500;
-    response.json({ message: error.message || "Failed to create a new blog" });
-  }
-};
+//     response.statusCode = 201;
+//     response.json(newBlog);
+//   } catch (error) {
+//     response.statusCode = 500;
+//     response.json({ message: error.message || "Failed to create a new blog" });
+//   }
+// };
