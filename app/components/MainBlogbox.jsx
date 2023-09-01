4fc21a6d846707cae137a5fbb4b01611a0ac1f21
real-StanYan <stanhavenoidea@gmail.com>
2023-09-01 16:50:29 +1000

    V1

--- app/components/MainBlogbox.jsx
+++ app/components/MainBlogbox.jsx
@@ -1,5 +1,32 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import "../css/mainblogbox.css";
 
-export default function MainBlogbox() {
-  return <div className="main-blogbox"></div>;
+export default function MainBlogbox({
+  title,
+  coverImg,
+  publishDate,
+  routerUrl,
+}) {
+  const router = useRouter();
+  return (
+    <div
+      className="main-blogbox"
+      onClick={() => {
+        router.push(`${routerUrl}`);
+      }}
+    >
+      <Image
+        src={coverImg}
+        className="main-blog-cover-img"
+        width={1000}
+        height={1000}
+      />
+      <div className="main-blog-des">
+        <h1>{title}</h1>
+        <p>{publishDate}</p>
+      </div>
+    </div>
+  );
 }
